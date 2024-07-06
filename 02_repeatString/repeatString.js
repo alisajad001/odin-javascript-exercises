604f4b0ca08508d2498ba8c009f3944e1483e692
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-07-06 14:51:44 +0430

    Add solution to assignment 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (textToRepeat, repeatTimes) {
+  let repeatedText = "";
 
+  for (let i = 1; i <= repeatTimes; i++) {
+    repeatedText += textToRepeat;
+  }
+
+  return repeatedText;
 };
 
 // Do not edit below this line
