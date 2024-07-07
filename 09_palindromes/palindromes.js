6df053bf04bad2d8a34f83d1e80dd17adeb78771
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-07-07 16:10:25 +0430

    Add solution to assignment 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const validCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanString = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => validCharacters.includes(char))
+    .join("");
+
+  const reverseString = cleanString.split("").reverse().join("");
+
+  return string === reverseString;
 };
+
+console.log(palindromes("mom"));
+console.log(palindromes("dad"));
 
 // Do not edit below this line
 module.exports = palindromes;
